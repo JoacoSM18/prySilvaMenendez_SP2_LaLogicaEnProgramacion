3f848ea3b1b6179f4a5c0fa74440894185983ff4
JoacoSM18 <joako.sm.18@gmail.com>
2025-09-26 09:30:50 -0300

    Agregar archivos de proyecto.

---
+++ prySilvaMenendez_SP2_LaLogicaEnProgramacion/frmRegistroCabañas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySilvaMenendez_SP2_LaLogicaEnProgramacion
+{
+    public partial class frmRegistroCabañas : Form
+    {
+        public frmRegistroCabañas()
+        {
+            InitializeComponent();
+        }
+    }
+}
